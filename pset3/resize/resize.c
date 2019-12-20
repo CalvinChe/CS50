b862093121a2507174d5b52ffe4f8c0a6cac1d34
CalvinChe <Calvinkpche@gmail.com>
2019-12-20 00:38:17 -0800

    fix resize.c

--- pset3/resize/resize.c
+++ pset3/resize/resize.c
@@ -14,7 +14,8 @@
  *     2. The first argument ,f , must be a floating-point value in the range [0.0, 100.0]
  *     3. The second argument must be the name of a BMP to be resized.
  *     4. The third argument must be the name of the resized version to be written.
- *     5. If your program is not executed with such, it should remind the user of the correct usage, as with print, and main should return 1;
+ *     5. If your program is not executed with such, it should remind the user of the correct usage, as with print
+ *        and main should return 1;
  *     6. If your program uses malloc, it must not leak any memory.
  * 
  * Example
